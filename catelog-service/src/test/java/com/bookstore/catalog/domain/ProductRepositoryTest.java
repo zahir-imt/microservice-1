eea887d0a9911e94bdaeeb53fe9fc827579fe8e9
zahir-imt <zahir_wp@yahoo.ca>
2026-02-03 12:47:18 -0500

    code formatted

--- catelog-service/src/test/java/com/bookstore/catalog/domain/ProductRepositoryTest.java
+++ catelog-service/src/test/java/com/bookstore/catalog/domain/ProductRepositoryTest.java
@@ -1,32 +1,29 @@
 package com.bookstore.catalog.domain;
 
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
-import com.bookstore.catalog.TestcontainersConfiguration;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.jdbc.Sql;
 
-import java.util.List;
-
-@DataJpaTest(properties = {
-        "spring.test.database.replace=none",
-        "spring.datasource.url=jdbc:tc:postgresql:18-alpine:///db",
-})
+@DataJpaTest(
+        properties = {
+            "spring.test.database.replace=none",
+            "spring.datasource.url=jdbc:tc:postgresql:18-alpine:///db",
+        })
 @Sql("/test-data.sql")
-//@Import(TestcontainersConfiguration.class)
+// @Import(TestcontainersConfiguration.class)
 class ProductRepositoryTest {
 
-    @Autowired ProductRepository productRepository;
+    @Autowired
+    ProductRepository productRepository;
 
     @Test
-    void shouldGetAllProducts(){
-        List<ProductEntity> list=  productRepository.findAll();
+    void shouldGetAllProducts() {
+        List<ProductEntity> list = productRepository.findAll();
         assertThat(list).hasSize(15);
     }
-
 }
