a31ff197a2844c0b13f1b7f6daa8f8123300a527
zahir-imt <zahir_wp@yahoo.ca>
2026-02-03 12:46:46 -0500

    code formatted

--- catelog-service/src/test/java/com/bookstore/catalog/domain/ProductRepositoryTest.java
+++ catelog-service/src/test/java/com/bookstore/catalog/domain/ProductRepositoryTest.java
@@ -1,8 +1,32 @@
 package com.bookstore.catalog.domain;
 
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.bookstore.catalog.TestcontainersConfiguration;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.jdbc.Sql;
 
-@DataJpaTest
-class ProductRepositoryTest {}
+import java.util.List;
+
+@DataJpaTest(properties = {
+        "spring.test.database.replace=none",
+        "spring.datasource.url=jdbc:tc:postgresql:18-alpine:///db",
+})
+@Sql("/test-data.sql")
+//@Import(TestcontainersConfiguration.class)
+class ProductRepositoryTest {
+
+    @Autowired ProductRepository productRepository;
+
+    @Test
+    void shouldGetAllProducts(){
+        List<ProductEntity> list=  productRepository.findAll();
+        assertThat(list).hasSize(15);
+    }
+
+}
