fc1630e369e25a8edd840a41773e57672acd6a74
zahir-imt <zahir_wp@yahoo.ca>
2026-02-18 14:57:16 -0500

    notification service

--- notification-service/src/main/java/com/bookstore/notification/ApplicationProperties.java
+++ notification-service/src/main/java/com/bookstore/notification/ApplicationProperties.java
@@ -2,6 +2,7 @@ package com.bookstore.notification;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+// Application properties
 @ConfigurationProperties(prefix = "notifications")
 public record ApplicationProperties(
         String orderEventsExchange,
